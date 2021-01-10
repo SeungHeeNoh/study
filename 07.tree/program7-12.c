4d10b0a1590f0b9984a58c8968947fdd4bf33f3c
nsh <nsh2398@naver.com>
2021-01-10 23:57:33 +0900

    chapter7 > tree > program 7.12 > 데이터 구조 선언 및 필드 선언, createNode및 compare함수 선언

--- 07.tree/program7-12.c
+++ 07.tree/program7-12.c
@@ -4,6 +4,31 @@
 #include<stdio.h>
 #include<string.h>
 #include<memory.h>
+#include<malloc.h>
+
+#define MAX_WORD_SIZE 100
+#define MAX_MEANING_SIZE 200
+typedef struct element {
+	char word[MAX_WORD_SIZE];
+	char meaning[MAX_MEANING_SIZE];
+}element;
+typedef struct TreeNode{
+	element item;
+	struct TreeNode *left, *right;
+}TreeNode;
+
+TreeNode *createNode(element item) {
+	TreeNode *new = (TreeNode *)malloc(sizeof(TreeNode));
+	new->item = item;
+	new->left = NULL;
+	new->right = NULL;
+
+	return new;
+}
+
+int compare(element e1, element e2) {
+	return strcmp(e1.word, e2.word);
+}
 
 void help() {
 	printf("*******************\n");
