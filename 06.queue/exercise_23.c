3228c8e2ca43a4a83a9519470dd85b2c7ec45c42
nsh <nsh2398@naver.com>
2021-01-15 23:41:21 +0900

    chapter6 > queue> exercise_23 > queue를 이용하여 피보나치 수열을 생성하도록 구현

--- 06.queue/exercise_23.c
+++ 06.queue/exercise_23.c
@@ -43,19 +43,47 @@ void enqueue(QueueType *q, QueueNode *new) {
 	}
 }
 
-void dequeue(QueueType *q) {
+int dequeue(QueueType *q) {
+	int data = 0;
+
 	if (q->front == NULL) {
 		printf("Queue is Empty");
 	} else {
 		QueueNode *removed;
 		removed = q->front;
 		q->front = removed->link;
+		data = removed->data;
 
 		if (isEmpty(q)) {
 			q->rear = NULL;
 		}
 		free(removed);
 	}
+
+	return data;
+}
+
+int peek(QueueType *q) {
+	int data = 0;
+	if (!isEmpty(q)) {
+		data = q->front->data;
+	}
+
+	return data;
+}
+
+int fib(QueueType *q, int data) {
+	int result = 0;
+
+	enqueue(q, createNode(0));
+	enqueue(q, createNode(1));
+
+	for (int i=0; i<data-1; i++) {
+		result = dequeue(q) + peek(q);
+		enqueue(q, createNode(result));
+	}
+
+	return result;
 }
 
 void display(QueueType *q) {
@@ -65,20 +93,24 @@ void display(QueueType *q) {
 		printf("%d ", p->data);
 		p = p->link;
 	}
+	printf("\n");
 }
 
 int main() {
+	printf("<test1>\n");
 	QueueType test1;
 	init(&test1);
+	printf("result = %d\n", fib(&test1, 5));
 
-	enqueue(&test1, createNode(10));
-	enqueue(&test1, createNode(22));
-	enqueue(&test1, createNode(3));
-	enqueue(&test1, createNode(10));
-	enqueue(&test1, createNode(18));
-	dequeue(&test1);
+	printf("<test2>\n");
+	QueueType test2;
+	init(&test2);
+	printf("result = %d\n", fib(&test2, 2));
 
+	printf("<test3>\n");
+	QueueType test3;
+	init(&test3);
+	printf("result = %d\n", fib(&test3, 17));
 
-	display(&test1);
 	return 0;
 }
