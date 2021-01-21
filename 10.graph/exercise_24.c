8c46cf8656ba43d2cf3ca2d15721994e794b80ba
nsh <nsh2398@naver.com>
2021-01-21 23:50:54 +0900

    chapter10 > graph > exercise 24 > Prim의 MST알고리즘을 이용하여 최소 신장 트리를 구하는 함수 구현

---
+++ 10.graph/exercise_24.c
@@ -0,0 +1,65 @@
+/*
+	Prim의 MST 알고리즘을 이용해서 최소 비용 신장 트리가 구성되는 과정을 보여라.
+	0번 정점으로 시작한다.
+*/
+#include<stdio.h>
+
+#define VERTICES 5
+#define INF 1000
+
+int selected[VERTICES];
+int distance[VERTICES];
+int weight[VERTICES][VERTICES] = {
+	{0, 1, INF, 2, INF}, 
+	{1, 0, 3, 1, 3},
+	{INF, 3, 0, INF, 1},
+	{2, 1, INF, 0, 2},
+	{INF, 3, 1, 2, 0}
+};
+
+void init() {
+	for(int i=0; i<VERTICES; i++){
+		selected[i]=0;
+		distance[i]=INF;
+	}
+}
+
+int getMinVertex(){
+	int v=0;
+
+	for(int i=0; i<VERTICES; i++){
+		if(!selected[i]) {
+			v=i;
+			break;
+		}
+	}
+	for(int i=0; i<VERTICES; i++){
+		if(!selected[i] && distance[i] < distance[v]) v=i;
+	}
+	return v;
+}
+
+
+void prim(int n){
+	int u;
+	init();
+
+	distance[n]=0;
+
+	for(int i=0; i<VERTICES; i++){
+		u=getMinVertex();
+		if (distance[u] == INF) return;
+		selected[u]=1;
+		printf("%d ", u);
+
+		for(int j=0; j<VERTICES; j++){
+			if(!selected[j] && weight[u][j] != INF && distance[j] > weight[u][j]){
+				distance[j] = weight[u][j];
+			}
+		}
+	}
+}
+
+void main(){
+	prim(0);
+}
