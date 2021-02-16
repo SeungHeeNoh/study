9f5cd77436984582361f8bd73190acba6ffff216
nsh <nsh2398@naver.com>
2021-02-16 14:54:52 +0900

    algorithm > algorithm_exercise_c > 06. hash table> exercise 01 > 선형 조사가 적용된 해시 테이블 구현

---
+++ algorithm/algorithm_exercise_c/06.hashTable/exercise_01.c
@@ -0,0 +1,98 @@
+/*
+	크기가 13인 해시 테이블이 해시 함수로 h(x) = x mod 13을 사용한다.
+	원소 10, 20, 30, 40, 33, 46, 50, 60
+	충돌 해결 : 개방주소 방식인 선형 조사
+*/
+
+#include<stdio.h>
+#define ARRAY_MAX 13
+#define INPUT_MAX 8
+
+typedef struct element {
+	int data;
+	int use;
+	int deleted;
+}element;
+
+void display(element array[]) {
+	for (int i=0; i<ARRAY_MAX; i++) {
+		printf("%d ", array[i].data);
+	}
+
+	printf("\n");
+}
+
+void initHashTable(element array[]) {
+	for (int i=0; i<ARRAY_MAX; i++) {
+		array[i].data = -1;
+		array[i].use = array[i].deleted = 0;
+	}
+}
+
+int getIndex(int data) {
+	return data % ARRAY_MAX;
+}
+
+void insertData(element r[], int data) {
+	int index = getIndex(data);
+
+	while(r[index].use){
+		index++;
+	}
+
+	r[index].data = data;
+	r[index].use = 1;
+	r[index].deleted = 0;
+}
+
+int findData(element r[], int data) {
+	int index = getIndex(data);
+
+	do {
+		if (r[index].data == data) {
+			return index;
+		}
+		index = ++index % ARRAY_MAX;
+	} while(r[index].use || r[index].deleted);
+
+	return -1;
+}
+
+void searchData(element r[], int data) {
+	int index = findData(r, data);
+
+	if (index < 0) {
+		printf("%d is not exist.\n", data);
+	} else {
+		printf("%d is exist.\n", data);
+	}
+}
+
+void deleteData(element r[], int data) {
+	int index = findData(r, data);
+
+	if (index < 0) {
+		printf("%d is not exist.\n", data);
+	} else {
+		r[index].data = -1;
+		r[index].use = 0;
+		r[index].deleted = 1;
+	}
+}
+
+void main() {
+	int test1[INPUT_MAX] = {10, 20, 30, 40, 33, 46, 50, 60};
+	element result1[ARRAY_MAX];
+	initHashTable(result1);
+
+	for (int i=0; i<INPUT_MAX; i++) {
+		insertData(result1, test1[i]);
+	}
+
+	display(result1);
+
+	searchData(result1, 60);
+	deleteData(result1, 46);
+	display(result1);
+	searchData(result1, 73);
+}
