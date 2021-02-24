8281feba9b32da33467241d55d1733d6ea52755d
nsh <nsh2398@naver.com>
2021-02-24 20:01:30 +0900

    programmers_challenges > js > 소수 찾기

---
+++ codingTest/programmers_challenges/js/find_prime_number.js
@@ -0,0 +1,24 @@
+function solution(n) {
+	var answer = 0, sqrt = parseInt(Math.sqrt(n)), arr = new Array(n+1);
+		// arr = Array.apply(null, new Array(n+1)).map(Number.prototype.valueOf,1);
+
+	for (var i=2; i<=n; i++) {
+		arr[i] = 1;
+	}
+
+	for (var k=2; k<=sqrt; k++) {
+		for (var j=k*2; j<=n; j += k) {
+			arr[j] = 0;
+		}
+	}
+
+	for (var i=2; i<=n; i++) {
+		if (arr[i] != 0) {
+			answer++;
+		}
+	}
+
+	return answer;
+}
+
+// 출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
