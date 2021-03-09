5d998cb63350e3049d9b4f1c3c4d3a2d8b5f9438
nsh <nsh2398@naver.com>
2021-03-09 02:30:21 +0000

    a행렬 곱셈 순서를 출력하는 함구현

--- algorithm/algorithm_exercise_c/08.DynamicProgramming/exercise_01.c
+++ algorithm/algorithm_exercise_c/08.DynamicProgramming/exercise_01.c
@@ -5,31 +5,54 @@
 #include<stdio.h>
 #define ARRAY_SIZE 6
 
-int minimum_mult(int d[]) {
+int minimum_mult(int d[], int minOrder[][ARRAY_SIZE]) {
 	int map[ARRAY_SIZE][ARRAY_SIZE];
 
 	for (int diagonal=0; diagonal<ARRAY_SIZE; diagonal++) {
 		for (int i=1; i<ARRAY_SIZE-diagonal; i++) {
 			int j=i+diagonal,
-				min = 100000000, temp;
+				min = 100000000, minIdx, temp;
 
 			if (i == j) {
 				map[i][j] = 0;
 			} else {
 				for(int k=i; k<j; k++) {
 					temp = map[i][k] + map[k+1][j] + d[i-1] * d[k] * d[j];
-					min = temp < min ? temp : min;
+					if (temp < min) {
+						min = temp;
+						minIdx = k;
+					}
 				}
 				map[i][j] = min;
+				minOrder[i][j] = minIdx;
 			}
 		}
 	}
 	return map[1][ARRAY_SIZE-1];
 }
 
+void order(int minOrder[][ARRAY_SIZE], int i, int j) {
+	int k;
+
+	if (i == j) {
+		printf("A%d", i);
+	} else {
+		k = minOrder[i][j];
+		printf("(");
+		order(minOrder, i, k);
+		printf("*");
+		order(minOrder, k+1, j);
+		printf(")");
+	}
+}
+
 int main() {
 	int p[ARRAY_SIZE] = {10, 5, 20, 7, 15, 3};
-	printf("%d", minimum_mult(p));
+	int minOrder[ARRAY_SIZE][ARRAY_SIZE];
+
+	printf("minimum multiplication : %d\n", minimum_mult(p, minOrder));
+	printf("order : ");
+	order(minOrder, 1, ARRAY_SIZE-1);
 
 	return 0;
 }
