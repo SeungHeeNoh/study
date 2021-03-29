7217e3c4352b47a047029ed09375a29cc53bd52d
nsh <nsh2398@naver.com>
2021-03-29 17:22:31 +0900

    Coding test > 고양이

---
+++ codingTest/baekjoon/java/step_01/src/step_01/exercise_03.java
@@ -0,0 +1,14 @@
+/*
+ * ===문제 출처===
+ * 10171번 고양이, <https://www.acmicpc.net/problem/10171>.
+ * */
+package step_01;
+
+public class exercise_03 {
+	public static void main() {
+		System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+	}
+}
