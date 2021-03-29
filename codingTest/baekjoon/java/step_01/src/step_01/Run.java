f05a44bce24134bc59ec6175d1e3e346700e5e99
nsh <nsh2398@naver.com>
2021-03-29 21:57:37 +0900

    Coding test > create step1 main

---
+++ codingTest/baekjoon/java/step_01/src/step_01/Run.java
@@ -0,0 +1,27 @@
+package step_01;
+
+public class Run {
+	public static void main(String args[]) {
+//			exercise_02 runner = new exercise_02();
+//			runner.main();
+//		
+//			exercise_03.main();
+//			
+//			exercise_04 runner = new exercise_04();
+//			runner.main();
+//			
+//			exercise_05.main();
+//			
+//			exercise_06.main();
+//			
+//			exercise_07.main();
+//			
+//			exercise_08.main();
+//			
+//			exercise_09.main();
+//			
+//			exercise_10.main();
+//			
+//			exercise_11.main();
+	}
+}
