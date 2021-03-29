5539aa4ff300203eb347cb4cc8caa7584c855b50
nsh <nsh2398@naver.com>
2021-03-29 20:32:20 +0900

    Coding test > 사칙연산

---
+++ codingTest/baekjoon/java/step_01/src/step_01/exercise_09.java
@@ -0,0 +1,30 @@
+/*
+ * ===문제 출처===
+ * 10869번 사칙연산, <https://www.acmicpc.net/problem/10869>.
+ * */
+package step_01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class exercise_09 {
+	public static void main() {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		try {
+			String arr[] = br.readLine().split(" ");
+			int a = Integer.parseInt(arr[0]),
+				b = Integer.parseInt(arr[1]);
+			
+			System.out.println(a+b);
+			System.out.println(a-b);
+			System.out.println(a*b);
+			System.out.println(a/b);
+			System.out.println(a%b);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
