480a6a3d999152bbec86e48f0a1ba14f6767e435
nsh <nsh2398@naver.com>
2021-04-01 22:04:24 +0900

    Coding test > StringBuilder class를 이용한 속도개선

--- codingTest/baekjoon/java/step_03/src/Exercise_08.java
+++ codingTest/baekjoon/java/step_03/src/Exercise_08.java
@@ -5,27 +5,24 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Exercise_08 {
 	public void main() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
 
 		int length = Integer.parseInt(br.readLine());
 		int a, b;
 		for(int i=1; i<=length; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());
 			a = Integer.parseInt(st.nextToken());
 			b = Integer.parseInt(st.nextToken());
 
-			bw.write("Case #" + i + ": " + a + " + " + b + " = " + (a+b) + "\n");
+			sb.append("Case #").append(i).append(": ").append(a).append(" + ").append(b).append(" = ").append(a+b).append("\n");
 		}
-
 		br.close();
-		bw.flush();
-		bw.close();
+		System.out.println(sb);
 	}
 }
