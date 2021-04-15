c396169f9a5b66f71a5601c010451ec08f407a23
nsh <nsh2398@naver.com>
2021-04-15 11:30:28 +0900

    Coding test > no.2775 부녀회장이 될테야

---
+++ codingTest/baekjoon/java/step_08/src/Exercise_06.java
@@ -0,0 +1,39 @@
+/*
+ * ===문제 출처===
+ * 2775 부녀회장이 될테야, <https://www.acmicpc.net/problem/2775>.
+ * */ 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Exercise_06 {
+	// ================== 132ms ==================
+	public void run() throws IOException{
+		int apt[][] = new int[15][14];
+		setPeople(apt);
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int length = Integer.parseInt(br.readLine());
+		while(length != 0) {
+			int k = Integer.parseInt(br.readLine()),
+				n = Integer.parseInt(br.readLine());
+			System.out.println(apt[k][n-1]);
+			length--;
+		}
+		br.close();
+	}
+
+	public void setPeople(int apt[][]) {
+		for(int i=0; i<apt.length; i++) {
+			for(int j=0; j<apt[i].length; j++) {
+				if(i == 0) {
+					apt[i][j] = j+1;
+				}else if(j == 0) {
+					apt[i][j] = 1;
+				}else {
+					apt[i][j] = apt[i][j-1] + apt[i-1][j];
+				}
+			}
+		}
+	}
+}
