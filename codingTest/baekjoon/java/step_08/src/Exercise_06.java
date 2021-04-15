ed4f7c02075829cd2694bdf7e3b418dcdd3a3f34
nsh <nsh2398@naver.com>
2021-04-15 11:31:29 +0900

    Coding test > no.2775 부녀회장이 될테야 - 재귀함수로 구현

--- codingTest/baekjoon/java/step_08/src/Exercise_06.java
+++ codingTest/baekjoon/java/step_08/src/Exercise_06.java
@@ -36,4 +36,35 @@ public class Exercise_06 {
 			}
 		}
 	}
+	// ================== 140ms ==================
+	// public void run() throws IOException{
+	// 	int apt[][] = new int[15][14];
+	// 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	// 	int length = Integer.parseInt(br.readLine());
+	// 	while(length != 0) {
+	// 		int k = Integer.parseInt(br.readLine()),
+	// 			n = Integer.parseInt(br.readLine());
+	// 		System.out.println(setPeople(apt, k, n-1));
+	// 		length--;
+	// 	}
+	// 	br.close();
+	// }
+
+	// public int setPeople(int apt[][], int i, int j) {
+	// 	int count = 0;
+
+	// 	if(apt[i][j] != 0) {
+	// 		count = apt[i][j];
+	// 	}else {
+	// 		if(i == 0) {
+	// 			apt[i][j] = j+1;
+	// 		}else if(j == 0) {
+	// 			apt[i][j] = 1;
+	// 		}else {
+	// 			apt[i][j] = setPeople(apt, i, j-1) + setPeople(apt, i-1, j);
+	// 		}
+	// 		count = apt[i][j];
+	// 	}
+	// 	return count;
+	// }
 }
