207df9ddfbf058a08ed88f6287b12b2a677c6eca
nsh <nsh2398@naver.com>
2021-04-20 22:48:12 +0900

    Coding test > 내/외각의 합/angle - main영역 주석 처리

--- codingTest/dovelet/step_01/src/Angle.java
+++ codingTest/dovelet/step_01/src/Angle.java
@@ -6,14 +6,14 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// public class Main {
+// 	public static void main(String[] args) throws IOException {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		Angle run = new Angle(Integer.parseInt(br.readLine()));
-		run.printResult();
-	}
-}
+// 		Angle run = new Angle(Integer.parseInt(br.readLine()));
+// 		run.printResult();
+// 	}
+// }
 
 class Angle {
 	int n;
