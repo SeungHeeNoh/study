fca221ade6ea59185689608f3eefe56698e1d3b6
nsh <nsh2398@naver.com>
2021-04-27 00:36:29 +0900

    Coding test > 팀 구성/fill - Player클래스 선언을 통해 기존 조건문 영역을 반복문으로 변경

--- codingTest/dovelet/step_02/src/Fill.java
+++ codingTest/dovelet/step_02/src/Fill.java
@@ -6,19 +6,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import static java.lang.Float.*;
 
 // public class Main {
 // 	public static void main(String[] args) throws IOException{
 // 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 // 		StringTokenizer st = new StringTokenizer(br.readLine());
-// 		System.out.println(new Fill(Float.parseFloat(st.nextToken()), Float.parseFloat(st.nextToken()), Float.parseFloat(st.nextToken())).getResult());
+// 		System.out.println(new Fill(parseFloat(st.nextToken()), parseFloat(st.nextToken()), parseFloat(st.nextToken())).getResult());
 // 		br.close();
-
 // 	}
 // }
 
 class Fill {
 	float speed, weight, power;
+
+	Player playerArr[] = {
+		new Player("Wide Receiver", 4.5f, 150f, 200f),
+		new Player("Lineman", 6.0f, 300f, 500f),
+		new Player("Quarterback", 5.0f, 200f, 300f)
+	};
 
 	public Fill() {}
 
@@ -31,26 +37,31 @@ class Fill {
 	public String getResult() {
 		StringBuilder sb = new StringBuilder();
 
-		if(speed <= 4.5 && weight >= 150 && power >= 200) {
-			sb.append("Wide Receiver");
-		}
-
-		if(speed <= 6.0 && weight >= 300 && power >= 500) {
-			if(!sb.toString().equals("")) {
-				sb.append(" ");
+		for(int i=0; i<playerArr.length; i++) {
+			if(speed <= playerArr[i].speed && weight >= playerArr[i].weight && power >= playerArr[i].power) {
+				if(!sb.toString().equals("")) {
+					sb.append(" ");
+				}
+				sb.append(playerArr[i].position);
 			}
-			sb.append("Lineman");
-		}
-
-		if(speed <= 5.0 && weight >= 200 && power >= 300) {
-			if(!sb.toString().equals("")) {
-				sb.append(" ");
-			}
-			sb.append("Quarterback");
 		}
 
 		if(sb.toString().equals("")) sb.append("No positions");
 
 		return sb.toString();
+	}
+}
+
+class Player {
+	String position;
+	float speed, weight, power;
+
+	Player() {}
+
+	Player(String position, float speed, float weight, float power) {
+		this.position = position;
+		this.speed = speed;
+		this.weight = weight;
+		this.power = power;
 	}
 }
