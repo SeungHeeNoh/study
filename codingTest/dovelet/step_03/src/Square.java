71277b52c972bb5e24dd831f3b6d100c3a26ea00
nsh <nsh2398@naver.com>
2021-05-10 21:32:57 +0900

    Coding test > 완전 제곱수/square

---
+++ codingTest/dovelet/step_03/src/Square.java
@@ -0,0 +1,51 @@
+/*
+ * ===문제 출처===
+ * 6174, <http://220.89.64.243/30stair/6174/6174.php?pname=6174>.
+ * */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import static java.lang.Integer.parseInt;
+
+// public class Main {
+// 	public static void main(String[] args) {
+// 		try(BufferedReader input = new BufferedReader(new InputStreamReader(System.in))) {
+// 			System.out.println(new Square(parseInt(input.readLine().trim()), parseInt(input.readLine().trim())).getResult());
+// 		}catch(IOException e) {
+// 			e.printStackTrace();
+// 		}
+// 	}
+// }
+
+class Square {
+	int m, n;
+
+	public Square() {}
+
+	public Square(int m, int n) {
+		this.m = m;
+		this.n = n;
+	}
+
+	public String getResult() {
+		StringBuffer result = new StringBuffer();
+		int sum = 0, min = 0;
+
+		for(int i=m; i<=n; i++) {
+			if(i % Math.sqrt(i) == 0) {
+				sum+= i;
+				if(min == 0) {
+					min = i;
+				}
+			}
+		}
+
+		if(sum != 0) {
+			result.append(sum).append("\n").append(min);
+		}else {
+			result.append(-1);
+		}
+		
+		return result.toString();
+	}
+}
