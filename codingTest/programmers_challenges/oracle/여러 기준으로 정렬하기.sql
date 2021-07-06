79bc4dc7d4239f5ce366c5149f98eeec786170c3
nsh <nsh2398@naver.com>
2021-07-06 22:22:17 +0900

    Coding test > Oracle > 여러 기준으로 정렬하기

---
+++ codingTest/programmers_challenges/oracle/여러 기준으로 정렬하기.sql
@@ -0,0 +1,13 @@
+/*
+ * ===문제 출처===
+ * 여러 기준으로 정렬하기, <https://programmers.co.kr/learn/courses/30/lessons/59404>.
+ * */
+SELECT
+       ANIMAL_ID
+     , NAME
+     , DATETIME
+  FROM ANIMAL_INS
+ ORDER BY NAME, DATETIME DESC;
+ /*
+출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
+*/
