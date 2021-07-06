227f1b3b8fd821df4d372d02632371d0fb9c42d1
nsh <nsh2398@naver.com>
2021-07-06 22:18:26 +0900

    Coding test > Oracle > 모든 레코드 조회하기

---
+++ codingTest/programmers_challenges/oracle/모든 레코드 조회하기.sql
@@ -0,0 +1,11 @@
+/*
+ * ===문제 출처===
+ * 모든 레코드 조회하기, <https://programmers.co.kr/learn/courses/30/lessons/59034>.
+ * */
+SELECT
+        *
+  FROM ANIMAL_INS 
+ ORDER BY ANIMAL_ID;
+ /*
+출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
+*/
