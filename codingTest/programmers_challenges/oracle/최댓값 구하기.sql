1d5b38d3eaf7d32cacd548ed44c254c002ec41f6
nsh <nsh2398@naver.com>
2021-07-06 22:24:12 +0900

    Coding test > Oracle > 최댓값 구하기

---
+++ codingTest/programmers_challenges/oracle/최댓값 구하기.sql
@@ -0,0 +1,10 @@
+/*
+ * ===문제 출처===
+ * 최댓값 구하기, <https://programmers.co.kr/learn/courses/30/lessons/59415>.
+ * */
+SELECT
+       MAX(DATETIME)
+  FROM ANIMAL_INS;
+ /*
+출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
+*/
