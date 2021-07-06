111e4de36122ceb9486e58221265ad7860937737
nsh <nsh2398@naver.com>
2021-07-06 22:25:22 +0900

    Coding test > Oracle > 역순 정렬하기

---
+++ codingTest/programmers_challenges/oracle/역순 정렬하기.sql
@@ -0,0 +1,12 @@
+/*
+ * ===문제 출처===
+ * 역순 정렬하기, <https://programmers.co.kr/learn/courses/30/lessons/59035>.
+ * */
+SELECT
+       NAME
+     , DATETIME
+  FROM ANIMAL_INS
+ ORDER BY ANIMAL_ID DESC;
+ /*
+출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
+*/
