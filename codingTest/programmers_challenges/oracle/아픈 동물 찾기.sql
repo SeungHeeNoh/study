6fa88eef348152edc2c0ddd8577cc5ce6f13a608
nsh <nsh2398@naver.com>
2021-07-06 22:26:14 +0900

    Coding test > Oracle > 아픈 동물 찾기

---
+++ codingTest/programmers_challenges/oracle/아픈 동물 찾기.sql
@@ -0,0 +1,13 @@
+/*
+ * ===문제 출처===
+ * 아픈 동물 찾기, <https://programmers.co.kr/learn/courses/30/lessons/59036>.
+ * */
+SELECT
+       ANIMAL_ID
+     , NAME
+ FROM ANIMAL_INS
+WHERE INTAKE_CONDITION = 'Sick'
+ORDER BY ANIMAL_ID;
+ /*
+출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
+*/
