ed2045eaeccf08992824a1fe3e0c1ae0fc4bd929
nsh <nsh2398@naver.com>
2021-07-06 22:27:12 +0900

    Coding test > Oracle > 동물의 아이디와 이름

---
+++ codingTest/programmers_challenges/oracle/동물의 아이디와 이름.sql
@@ -0,0 +1,12 @@
+/*
+ * ===문제 출처===
+ * 동물의 아이디와 이름, <https://programmers.co.kr/learn/courses/30/lessons/59403>.
+ * */
+SELECT
+       ANIMAL_ID
+     , NAME
+  FROM ANIMAL_INS
+ ORDER BY ANIMAL_ID;
+ /*
+출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
+*/
