a261039a56199a298dc306a2176b9503f1fa7609
nsh <nsh2398@naver.com>
2021-07-08 23:38:53 +0900

    Coding test > Oracle > 동물 수 구하기

---
+++ codingTest/programmers_challenges/oracle/동물 수 구하기.sql
@@ -0,0 +1,10 @@
+/*
+ * ===문제 출처===
+ * 동물 수 구하기, <https://programmers.co.kr/learn/courses/30/lessons/59406>.
+ * */
+SELECT
+      COUNT(*)
+  FROM ANIMAL_INS;
+ /*
+출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
+*/
