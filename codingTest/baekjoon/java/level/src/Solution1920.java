a9f40c3c8b0a701d8220959091f8002fa5c04967
nsh <nsh2398@naver.com>
2022-01-19 22:01:45 +0900

    Coding test > Java > no.1920 수 찾기

---
+++ codingTest/baekjoon/java/level/src/Solution1920.java
@@ -0,0 +1,41 @@
+/*
+ * ===문제 출처===
+ * 1920 수 찾기, <https://www.acmicpc.net/problem/1920>.
+ * */ 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+// public class Main {
+// 	public static void main(String[] args) throws IOException {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		StringBuffer result = new StringBuffer();
+// 		Set<String> set = new HashSet<>();
+// 		br.readLine();
+// 		StringTokenizer st = new StringTokenizer(br.readLine());
+
+// 		while(st.hasMoreTokens()) {
+// 			set.add(st.nextToken());
+// 		}
+
+// 		br.readLine();
+// 		st = new StringTokenizer(br.readLine());
+
+// 		while(st.hasMoreTokens()) {
+// 			if(set.contains(st.nextToken())) {
+// 				result.append("1\n");
+// 			} else {
+// 				result.append("0\n");
+// 			}
+// 		}
+		
+// 		System.out.println(result.toString());
+// 	}
+// }
+
+class Solution1920 {
+
+}
