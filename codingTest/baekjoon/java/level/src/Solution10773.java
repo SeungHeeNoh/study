8b97d7c8db7889886e482136b0724b5fec166f62
nsh <nsh2398@naver.com>
2022-01-21 11:50:35 +0900

    Coding test > Java > no.10773 제로

---
+++ codingTest/baekjoon/java/level/src/Solution10773.java
@@ -0,0 +1,39 @@
+/*
+ * ===문제 출처===
+ * 10773 제로, <https://www.acmicpc.net/problem/10773>.
+ * */ 
+import static java.lang.Integer.parseInt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+// public class Main {
+// 	public static void main(String[] args) throws IOException {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		Stack<Integer> input = new Stack<>();
+// 		int k = parseInt(br.readLine());
+// 		int result = 0;
+
+// 		for(int i=0; i<k; i++) {
+// 			int number = parseInt(br.readLine());
+
+// 			if(number == 0) {
+// 				input.pop();
+// 			} else {
+// 				input.push(number);
+// 			}
+// 		}
+
+// 		while(!input.isEmpty()) {
+// 			result += input.pop();
+// 		}
+
+// 		System.out.println(result);
+// 	}
+// }
+
+class Solution10773 {
+
+}
