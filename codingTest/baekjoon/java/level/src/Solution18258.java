d538d5fbd8bf820c09b555a724e0aa48e7c46ef5
nsh <nsh2398@naver.com>
2022-01-22 13:28:47 +0900

    Coding test > Java > no.18258 큐 2 - 직접 구현

--- codingTest/baekjoon/java/level/src/Solution18258.java
+++ codingTest/baekjoon/java/level/src/Solution18258.java
@@ -7,7 +7,6 @@ import static java.lang.Integer.parseInt;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 // public class Main {
@@ -15,24 +14,24 @@ import java.util.StringTokenizer;
 // 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 // 		int test = parseInt(br.readLine());
 // 		StringBuffer answer = new StringBuffer();
-// 		LinkedList<Integer> queue = new LinkedList<>();
+// 		Solution18258 queue = new Solution18258(test);
 
 // 		while(test-- > 0) {
 // 			StringTokenizer st = new StringTokenizer(br.readLine());
 // 			String command = st.nextToken();
 
 // 			if(command.equals("push")) {
-// 				queue.add(parseInt(st.nextToken()));
+// 				queue.push(parseInt(st.nextToken()));
 // 			} else if(command.equals("pop")) {
-// 				answer.append(queue.isEmpty() ? -1 : queue.poll()).append("\n");
+// 				answer.append(queue.pop()).append("\n");
 // 			} else if(command.equals("size")) {
 // 				answer.append(queue.size()).append("\n");
 // 			} else if(command.equals("empty")) {
-// 				answer.append(queue.isEmpty() ? 1 : 0).append("\n");
+// 				answer.append(queue.isEmpty()).append("\n");
 // 			} else if(command.equals("front")) {
-// 				answer.append(queue.isEmpty() ? -1 : queue.getFirst()).append("\n");
+// 				answer.append(queue.front()).append("\n");
 // 			} else if(command.equals("back")) {
-// 				answer.append(queue.isEmpty() ? -1 : queue.getLast()).append("\n");
+// 				answer.append(queue.rear()).append("\n");
 // 			}
 // 		}
 		
@@ -41,5 +40,35 @@ import java.util.StringTokenizer;
 // }
 
 class Solution18258 {
+	private int[] queue;
+	private int front=0;
+	private int rear=0;
 
+	public Solution18258(int size) {
+		queue = new int[size];
+	}
+
+	public int isEmpty() {
+		return rear == front ? 1 : 0;
+	}
+
+	public void push(int value) {
+		queue[rear++] = value;
+	}
+
+	public int pop() {
+		return isEmpty() > 0 ? -1 : queue[front++];
+	}
+
+	public int size() {
+		return rear-front;
+	}
+
+	public int front() {
+		return isEmpty() > 0 ? -1 : queue[front];
+	}
+
+	public int rear() {
+		return isEmpty() > 0 ? -1 : queue[rear-1];
+	}
 }
