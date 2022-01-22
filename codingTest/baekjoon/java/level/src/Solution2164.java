f174ddbf9598733e96f735339bf3314544435dea
nsh <nsh2398@naver.com>
2022-01-22 14:42:33 +0900

    Coding test > Java > no.2164 카드2

---
+++ codingTest/baekjoon/java/level/src/Solution2164.java
@@ -0,0 +1,34 @@
+/*
+ * ===문제 출처===
+ * 2164 카드2, <https://www.acmicpc.net/problem/2164>.
+ * */ 
+import static java.lang.Integer.parseInt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+// public class Main {
+// 	public static void main(String[] args) throws IOException {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		int input = parseInt(br.readLine());
+// 		Queue<Integer> queue = new LinkedList<>();
+
+// 		for(int i=1; i<=input; i++) {
+// 			queue.offer(i);
+// 		}
+
+// 		while(queue.size() > 1) {
+// 			queue.poll();
+// 			queue.offer(queue.poll());
+// 		}
+		
+// 		System.out.println(queue.poll());
+// 	}
+// }
+
+class Solution2164 {
+
+}
