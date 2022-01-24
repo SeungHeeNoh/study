70e2099f1dfe27d38f2964bd264d4b758cc85b55
nsh <nsh2398@naver.com>
2022-01-24 10:28:24 +0900

    Coding test > Java > no.10816 숫자 카드 2

---
+++ codingTest/baekjoon/java/level/src/Solution10816.java
@@ -0,0 +1,38 @@
+/*
+ * ===문제 출처===
+ * 10816 숫자 카드 2, <https://www.acmicpc.net/problem/10816>.
+ * */ 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+// public class Main {
+// 	public static void main(String[] args)throws IOException {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		int n = Integer.parseInt(br.readLine());
+// 		StringTokenizer st = new StringTokenizer(br.readLine());
+// 		Map<Integer, Integer> inputs = new HashMap<>();
+// 		StringBuffer answer = new StringBuffer();
+		
+// 		for(int i=0; i<n; i++) {
+// 			int input = Integer.parseInt(st.nextToken());
+// 			inputs.put(input, inputs.getOrDefault(input, 0)+1);
+// 		}
+		
+// 		int m = Integer.parseInt(br.readLine());
+// 		st = new StringTokenizer(br.readLine());
+
+// 		for(int i=0; i<m; i++) {
+// 			answer.append(inputs.getOrDefault(Integer.parseInt(st.nextToken()), 0)).append(" ");
+// 		}
+
+// 		System.out.println(answer.toString());
+// 	}
+// }
+
+class Solution10816 {
+	
+}
