c370d4e004d24c422f19146f241ace254e7c68ee
nsh <nsh2398@naver.com>
2022-01-24 13:58:55 +0900

    Coding test > Java > no.1874 스택 수열

---
+++ codingTest/baekjoon/java/level/src/Solution1874.java
@@ -0,0 +1,43 @@
+/*
+ * ===문제 출처===
+ * 1874 스택 수열, <https://www.acmicpc.net/problem/1874>.
+ * */ 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+// public class Main {
+// 	public static void main(String[] args)throws IOException {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		StringBuilder answer = new StringBuilder();
+// 		Stack<Integer> stack = new Stack<>();
+// 		int n = Integer.parseInt(br.readLine());
+// 		int index = 1;
+
+// 		while(n-- > 0) {
+// 			int input = Integer.parseInt(br.readLine());
+
+// 			while(index <= input) {
+// 				stack.push(index);
+// 				answer.append("+\n");
+// 				index++;
+// 			}
+
+// 			if(stack.peek() != input) {
+// 				answer = new StringBuilder();
+// 				answer.append("NO");
+// 				break;
+// 			} else {
+// 				stack.pop();
+// 				answer.append("-\n");
+// 			}
+// 		}
+
+// 		System.out.println(answer);
+// 	}
+// }
+
+class Solution1874 {
+
+}
