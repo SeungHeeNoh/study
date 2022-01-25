b03b0d5b82c396a3b90ee6e7f072f3ed988969c2
nsh <nsh2398@naver.com>
2022-01-25 15:58:07 +0900

    Coding test > Java > no.9461 파도반 수열

---
+++ codingTest/baekjoon/java/level/src/Solution9461.java
@@ -0,0 +1,34 @@
+/*
+ * ===문제 출처===
+ * 9461 파도반 수열 <https://www.acmicpc.net/problem/9461>.
+ * */ 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// public class Main {
+// 	public static void main(String[] args)throws IOException {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		StringBuilder answer = new StringBuilder();
+// 		int test = Integer.parseInt(br.readLine());
+// 		long[] dp = new long[100];
+
+// 		for(int i=0; i<dp.length; i++) {
+// 			if(i < 3) {
+// 				dp[i] = 1;
+// 			} else {
+// 				dp[i] = dp[i-2] + dp[i-3];
+// 			}
+// 		}
+
+// 		while(test-- > 0) {
+// 			answer.append(dp[Integer.parseInt(br.readLine())-1]).append("\n");
+// 		}
+		
+// 		System.out.println(answer);
+// 	}
+// }
+
+class Solution9461 {
+
+}
