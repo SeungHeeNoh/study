de2d1d7a0b3494d1d2a013f01e81813bb5fbb51c
nsh <nsh2398@naver.com>
2022-01-26 17:29:08 +0900

    Coding test > Java > no.1152 단어의 개수

---
+++ codingTest/baekjoon/java/level/src/Solution1152.java
@@ -0,0 +1,21 @@
+/*
+ * ===문제 출처===
+ * 1152 단어의 개수, <https://www.acmicpc.net/problem/1152>.
+ * */ 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// public class Main {
+// 	public static void main(String[] args)throws IOException {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		StringTokenizer st = new StringTokenizer(br.readLine());
+
+// 		System.out.println(st.countTokens());
+// 	}
+// }
+
+class Solution1152 {
+
+}
