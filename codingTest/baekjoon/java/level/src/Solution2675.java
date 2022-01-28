80089e186a23d579fe72da9f0a0ea8f22921afc4
nsh <nsh2398@naver.com>
2022-01-28 11:54:15 +0900

    Coding test > Java > no.2675 문자열 반복

---
+++ codingTest/baekjoon/java/level/src/Solution2675.java
@@ -0,0 +1,33 @@
+/*
+ * ===문제 출처===
+ * 2675 문자열 반복, <https://www.acmicpc.net/problem/2675>.
+ * */ 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// public class Main {
+// 	public static void main(String[] args)throws IOException {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		StringBuilder answer = new StringBuilder();
+// 		int test = Integer.parseInt(br.readLine());
+
+// 		while(test-- > 0) {
+// 			StringTokenizer st = new StringTokenizer(br.readLine());
+// 			int n = Integer.parseInt(st.nextToken());
+// 			String input = st.nextToken();
+
+// 			for(int i=0; i<input.length() * n; i++) {
+// 				answer.append(input.charAt(i/n));
+// 			}
+// 			answer.append("\n");
+// 		}
+		
+// 		System.out.println(answer);
+// 	}
+// }
+
+class Solution2675 {
+
+}
