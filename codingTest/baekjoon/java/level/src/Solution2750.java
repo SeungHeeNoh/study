8fd355a4e3306332dbc43f39164affd98ddb6c88
nsh <nsh2398@naver.com>
2022-01-30 18:06:11 +0900

    Coding test > Java > no.2750 수 정렬하기

---
+++ codingTest/baekjoon/java/level/src/Solution2750.java
@@ -0,0 +1,31 @@
+/*
+ * ===문제 출처===
+ * 2750 수 정렬하기, <hhttps://www.acmicpc.net/problem/2750>.
+ * */ 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+// public class Main {
+// 	public static void main(String[] args)throws IOException {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		StringBuilder answer = new StringBuilder();
+// 		int n = Integer.parseInt(br.readLine());
+// 		PriorityQueue<Integer> heap = new PriorityQueue<>();
+
+// 		for(int i=0; i<n; i++) {
+// 			heap.offer(Integer.parseInt(br.readLine()));
+// 		}
+
+// 		while(!heap.isEmpty()) {
+// 			answer.append(heap.poll() + "\n");
+// 		}
+
+// 		System.out.println(answer);
+// 	}
+// }
+
+class Solution2750 {
+	
+}
