8cd09fc0e71b1c85dda2cd1204dce5efafaec7f5
nsh <nsh2398@naver.com>
2022-01-31 15:36:35 +0900

    Coding test > Java > no.10809 알파벳 찾기

---
+++ codingTest/baekjoon/java/level/src/Solution10809.java
@@ -0,0 +1,26 @@
+/*
+ * ===문제 출처===
+ * 10809 알파벳 찾기, <https://www.acmicpc.net/problem/10809>.
+ * */ 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// public class Main {
+// 	public static void main(String[] args)throws IOException {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		String input = br.readLine();
+// 		StringBuilder answer = new StringBuilder(26*3);
+
+// 		for(int i=0; i<26; i++) {
+// 			if(i != 0) answer.append(" ");
+// 			answer.append(input.indexOf(i+'a') < 0 ? -1 : input.indexOf(i+'a'));
+// 		}
+
+// 		System.out.println(answer);
+// 	}
+// }
+
+class Solution10809 {
+
+}
