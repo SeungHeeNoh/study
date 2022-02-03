80cc43b4748554ef6b263b6e7265a66a298783a6
nsh <nsh2398@naver.com>
2022-02-03 17:43:06 +0900

    Coding test > Java > no.11720 숫자의 합

---
+++ codingTest/baekjoon/java/level/src/Solution11720.java
@@ -0,0 +1,26 @@
+/*
+ * ===문제 출처===
+ * 11720 숫자의 합, <https://www.acmicpc.net/problem/11720>.
+ * */ 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// public class Main {
+// 	public static void main(String[] args) throws IOException {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		int n = Integer.parseInt(br.readLine());
+// 		String input = br.readLine();
+// 		int answer = 0;
+
+// 		for(int i=0; i<n; i++) {
+// 			answer += input.charAt(i)-'0';
+// 		}
+
+// 		System.out.println(answer);
+// 	}
+// }
+
+class Solution11720 {
+
+}
