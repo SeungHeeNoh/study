7adc6587ac6998c980c52ae5906433ff5ca6a222
nsh <nsh2398@naver.com>
2022-02-04 16:38:17 +0900

    Coding test > Java > no.10989 수 정렬하기 3

---
+++ codingTest/baekjoon/java/level/src/Solution10989.java
@@ -0,0 +1,34 @@
+/*
+ * ===문제 출처===
+ * 10989 수 정렬하기 3, <https://www.acmicpc.net/problem/10989>.
+ * */ 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// public class Main {
+// 	public static void main(String[] args) throws IOException {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		int n = Integer.parseInt(br.readLine());
+// 		int[] list = new int[10001];
+// 		StringBuilder answer = new StringBuilder(3*n);
+		
+// 		for(int i=0; i<n; i++) {
+// 			list[Integer.parseInt(br.readLine())]++;
+// 		}
+		
+// 		for(int i=1; i<list.length; i++) {
+// 			int count = list[i];
+
+// 			for(int j=1; j<=count; j++) {
+// 				answer.append(i + "\n");
+// 			}
+// 		}
+
+// 		System.out.println(answer);
+// 	}
+// }
+
+class Solution10989 {
+
+}
