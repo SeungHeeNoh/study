9bb9ed079636251951c759d02d13956f535a1aff
nsh <nsh2398@naver.com>
2022-02-08 16:32:48 +0900

    Coding test > Java > no.1037 약수

---
+++ codingTest/baekjoon/java/level/src/Solution1037.java
@@ -0,0 +1,36 @@
+/*
+ * ===문제 출처===
+ * 1037 약수, <https://www.acmicpc.net/problem/1037>.
+ * */ 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// public class Main {
+// 	public static void main(String[] args) throws IOException {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		int n = Integer.parseInt(br.readLine());
+// 		StringTokenizer st = new StringTokenizer(br.readLine());
+// 		int min = Integer.MAX_VALUE,
+// 			max = Integer.MIN_VALUE;
+
+// 		for(int i=0; i<n; i++) {
+// 			int input = Integer.parseInt(st.nextToken());
+
+// 			if(min > input) {
+// 				min = input;
+// 			}
+
+// 			if(max < input) {
+// 				max = input;
+// 			}
+// 		}
+
+// 		System.out.println(min*max);
+// 	}
+// }
+
+class Solution1037 {
+
+}
