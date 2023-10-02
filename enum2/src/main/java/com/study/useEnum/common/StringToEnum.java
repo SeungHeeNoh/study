47a45a68f15fbfe14cd6c66c44bae1806278471a
nsh <nsh2398@naver.com>
2023-10-02 15:26:27 +0900

    ﻿request param으로 전달받은 String을 Enum으로 받기 ② > AnimalConverter를 Bean으로 등록

--- enum2/src/main/java/com/study/useEnum/common/StringToEnum.java
+++ enum2/src/main/java/com/study/useEnum/common/StringToEnum.java
@@ -1,11 +1,15 @@
 package com.study.useEnum.common;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
 import com.study.useEnum.service.Animal;
 
+@Configuration
 public class StringToEnum {
 	
+	@Component
 	public static class AnimalConverter implements Converter<String, Animal> {
 
 		@Override
